3be698bca45a11854fc08648e11bf5b54692438f
marlonnn <zhongw_1988@126.com>
2016-07-20 15:00:51 +0800

    add mini dump demo project

---
+++ Summer/Demo.MiniDump/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo.MiniDump
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread;
+            thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.Start();
+        }
+
+        private void ThreadMethod()
+        {
+            throw new Exception("From new thread");
+        }
+    }
+}
---
+++ Summer/Demo.MiniDump/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo.MiniDump
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            MiniDump.Init();
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
