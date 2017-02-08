fdb0a8bc144873c538e63b069bdf63e17608bdfd
marlonnn <zhongw_1988@126.com>
2017-02-08 13:29:55 +0800

    创建Demo.DB程序

---
+++ Summer/Demo.DB/ADO/MainADO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Summer.System.Data;
+using Summer.System.Data.DbMapping;
+
+namespace Demo.DB.ADO
+{
+    public class MainADO : SmrAdoTmplate<TMain>
+    {
+
+    }
+
+    [TableAttribute("Main")]
+    public class TMain
+    {
+        [FieldAttribute("Key", PrimaryKey = true)]
+        public string Key;
+
+        [FieldAttribute("Start")]
+        public DateTime Start;
+
+        [FieldAttribute("End")]
+        public DateTime End;
+
+        [FieldAttribute("Note")]
+        public string Note;
+    }
+
+}
---
+++ Summer/Demo.DB/Program.cs
@@ -0,0 +1,30 @@
+﻿using Demo.DB.ADO;
+using Summer.System.Core;
+using System;
+using System.Windows.Forms;
+
+namespace Demo.DB
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                //Application.Run(new Form1());
+
+                var v = SpringHelper.GetObject<MainADO>("mainADO");
+            }
+            catch (Exception ee)
+            {
+
+            }
+        }
+    }
+}
